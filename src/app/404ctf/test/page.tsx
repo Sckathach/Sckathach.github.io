1c1d954742cfdc4bd16da8bb0a36cb8d4c68b8d0
Sckathach <sckathach@hackademint.org>
2024-07-18 21:14:47 +0200

    tests...

--- src/app/404ctf/test/page.tsx
+++ src/app/404ctf/test/page.tsx
@@ -3,11 +3,15 @@ import { useSearchParams } from 'next/navigation';
 
 export default function Index() {
   const searchParams = useSearchParams()
-  const item = searchParams.get('item')
+  const id = searchParams.get('id')
+  const solution = searchParams.get('solution')
+
+  const validId = id && parseInt(id as string) >= 1 && parseInt(id as string) <= 4 
+  const iframeUrl = solution ? `./about/challenge_${validId}_solution` : `./about/challenge_${validId}.html`
 
   return(
     <>
-    <h1>{item}</h1>
+    <h1>{iframeUrl}</h1>
     </>
   )
 }
